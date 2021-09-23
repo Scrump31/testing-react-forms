745c5fe117a8809aee79b2336559b26f0b428bea
Scottie Crump <sncrump29@gmail.com>
2021-09-23 10:49:34 -0400

    test: update snapshot for RadioButton component

--- components/RadioButton.test.tsx
+++ components/RadioButton.test.tsx
@@ -1,5 +1,5 @@
-import React from 'react'
 import { render } from '@testing-library/react'
+import React from 'react'
 import RadioButton from './RadioButton'
 
 describe('<RadioButton />', () => {
@@ -16,103 +16,27 @@ describe('<RadioButton />', () => {
         handleChange={handleChange}
       />
     )
-    expect(radioButton).toMatchInlineSnapshot(`
-      Object {
-        "asFragment": [Function],
-        "baseElement": <body>
-          <div>
-            <div
-              class="text-gray-700 text-sm sm:text-2xl font-bold mb-1"
-            >
-              <input
-                checked=""
-                data-testid="excellent"
-                id="excellent"
-                name="rating"
-                type="radio"
-                value="excellent"
-              />
-              <label
-                class="ml-2"
-                for="excellent"
-              >
-                Excellent
-              </label>
-            </div>
-          </div>
-        </body>,
-        "container": <div>
-          <div
-            class="text-gray-700 text-sm sm:text-2xl font-bold mb-1"
+    expect(radioButton.container).toMatchInlineSnapshot(`
+      <div>
+        <div
+          class="text-gray-700 text-sm sm:text-2xl font-bold mb-1"
+        >
+          <input
+            checked=""
+            data-testid="excellent"
+            id="excellent"
+            name="rating"
+            type="radio"
+            value="excellent"
+          />
+          <label
+            class="ml-2"
+            for="excellent"
           >
-            <input
-              checked=""
-              data-testid="excellent"
-              id="excellent"
-              name="rating"
-              type="radio"
-              value="excellent"
-            />
-            <label
-              class="ml-2"
-              for="excellent"
-            >
-              Excellent
-            </label>
-          </div>
-        </div>,
-        "debug": [Function],
-        "findAllByAltText": [Function],
-        "findAllByDisplayValue": [Function],
-        "findAllByLabelText": [Function],
-        "findAllByPlaceholderText": [Function],
-        "findAllByRole": [Function],
-        "findAllByTestId": [Function],
-        "findAllByText": [Function],
-        "findAllByTitle": [Function],
-        "findByAltText": [Function],
-        "findByDisplayValue": [Function],
-        "findByLabelText": [Function],
-        "findByPlaceholderText": [Function],
-        "findByRole": [Function],
-        "findByTestId": [Function],
-        "findByText": [Function],
-        "findByTitle": [Function],
-        "getAllByAltText": [Function],
-        "getAllByDisplayValue": [Function],
-        "getAllByLabelText": [Function],
-        "getAllByPlaceholderText": [Function],
-        "getAllByRole": [Function],
-        "getAllByTestId": [Function],
-        "getAllByText": [Function],
-        "getAllByTitle": [Function],
-        "getByAltText": [Function],
-        "getByDisplayValue": [Function],
-        "getByLabelText": [Function],
-        "getByPlaceholderText": [Function],
-        "getByRole": [Function],
-        "getByTestId": [Function],
-        "getByText": [Function],
-        "getByTitle": [Function],
-        "queryAllByAltText": [Function],
-        "queryAllByDisplayValue": [Function],
-        "queryAllByLabelText": [Function],
-        "queryAllByPlaceholderText": [Function],
-        "queryAllByRole": [Function],
-        "queryAllByTestId": [Function],
-        "queryAllByText": [Function],
-        "queryAllByTitle": [Function],
-        "queryByAltText": [Function],
-        "queryByDisplayValue": [Function],
-        "queryByLabelText": [Function],
-        "queryByPlaceholderText": [Function],
-        "queryByRole": [Function],
-        "queryByTestId": [Function],
-        "queryByText": [Function],
-        "queryByTitle": [Function],
-        "rerender": [Function],
-        "unmount": [Function],
-      }
+            Excellent
+          </label>
+        </div>
+      </div>
     `)
   })
 })
